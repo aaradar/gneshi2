ce7d1948b92138f35d11c862ed64c966c5050d13
Alexander Radar <alexander.radar11@gmail.com>
2024-11-15 12:14:03 -0500

    yes

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
+// Replace `<your-repo-name>` with your actual repository name
 export default defineConfig({
   plugins: [react()],
-  base: "//", // Set to the name of your repository
 });
