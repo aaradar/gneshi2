5e9686b5ffd11345de0d0631f57c5f609d5a4875
Alexander Radar <alexander.radar11@gmail.com>
2024-11-15 12:02:17 -0500

    attempt2

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/gneshi2/", // Replace 'your-repo-name' with your GitHub repository name
+  base: "//", // Set to the name of your repository
 });
