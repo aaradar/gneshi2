330912876cc1f0dee1f784562417864c7c410988
Alexander Radar <alexander.radar11@gmail.com>
2024-11-15 12:15:53 -0500

    somehow i lost

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // Replace `<your-repo-name>` with your actual repository name
 export default defineConfig({
+  base: "/gneshi2/", // replace with the path where your app is deployed
   plugins: [react()],
 });
